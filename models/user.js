1adcd0d0f0974efe1760ee0d0c33cc59d8d5f858
Anton Taran <91492704+ToshaUA@users.noreply.github.com>
2023-03-16 20:36:55 +0200

    hw4

---
+++ models/user.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      minlenght: 6,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registrSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const schema = {
+  registrSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schema,
+};
---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { validateBody, authenticate } = require("../../middlewares");
+const { schema } = require("../../models/user");
+const ctrl = require("../../controllers/auth");
+
+router.post("/signup", validateBody(schema.registrSchema), ctrl.register);
+router.post("/login", validateBody(schema.loginSchema), ctrl.login);
+router.post("/logout", authenticate, ctrl.logout);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+module.exports = router;
